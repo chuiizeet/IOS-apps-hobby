f883a61e44af2f1a2b1183933a4a1883f874c415
chuiizeet <chuy.guerra19@outlook.es>
2019-05-02 11:36:33 -0500

    Setup

---
+++ xyz/xyz/xyz/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  xyz
+//
+//  Created by imac on 5/2/19.
+//  Copyright © 2019 JeguLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var xLbl: UILabel!
+    @IBOutlet weak var yLbl: UILabel!
+    @IBOutlet weak var zLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
